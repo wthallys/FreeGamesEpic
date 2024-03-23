8483777ccd17e012233b26286180f10c7d862bd7
Wyddenberg Thallys <wthallys.02@gmail.com>
2024-03-23 12:10:35 -0300

    docs: update documentation

--- src/main/java/com/wthallys/freeGamesEpic/controller/GameController.java
+++ src/main/java/com/wthallys/freeGamesEpic/controller/GameController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/freegames")
+@RequestMapping("/api/games")
 public class GameController {
     @Autowired
     private GameService gameService;
